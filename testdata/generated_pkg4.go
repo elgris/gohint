efdc818dfca41fb8a6a24ae6ef6db4705a993f89
Evgeny Danilenko <eugene.danilenko@lazada.com>
2015-06-30 18:38:48 +0300

    Check for generated content in file

---
+++ testdata/generated_pkg4.go
@@ -0,0 +1,8 @@
+/*
+Package foo is pretty sweet.
+*/
+package foo
+
+//go:generate msgpgen4
+
+var myZeroInt int = 0 // OK
---
+++ testdata/generated_pkg5.go
@@ -0,0 +1,7 @@
+package datastruct
+
+// NOTE: THIS FILE WAS PRODUCED BY THE5
+// MSGP CODE GENERATION TOOL (github.com/tinylib/msgp)
+// DO NOT EDIT
+
+var myZeroInt int = 0         // OK
